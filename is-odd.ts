3b3ce807dfde41f4765d642e18cef126d9007f3d
Hoyasumii <alanreisanjo@gmail.com>
2025-03-11 23:57:25 -0300

    Added JSDOCS

--- is-odd.ts
+++ is-odd.ts
@@ -1,3 +1,8 @@
+/**
+ * A method who verify if the value is odd
+ * @param {number} value - The value to verify
+ * @returns {boolean} **true** if the *value* is odd; **false** if the *value* is even;
+ **/
 export function isOdd(value: number): boolean {
   return value % 2 === 1;
 }
